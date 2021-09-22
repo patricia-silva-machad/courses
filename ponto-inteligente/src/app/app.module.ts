a0e6e18c68f1a123928c6c6ce30f694426a9606a
Patricia Machado <88860311+patricia-silva-machad@users.noreply.github.com>
2021-09-21 23:12:04 -0300

    adicionando API Restful Java/Spring disponibilizada pelo instrutor e iniciando estilização do front

---
+++ ponto-inteligente/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
